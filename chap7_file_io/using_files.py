ffcc720d4940013aeb3bafa5221bdec50be0d079
xiaodingding <rannuofli@gmail.com>
2024-08-26 10:43:37 +0200

    write file xmen

--- chap7_file_io/using_files.py
+++ chap7_file_io/using_files.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python3.11
-print("gogog")
+my_file = open("xmen.txt",'w+')
+my_file.write('beast\n')
+my_file.write('phoenix\n')
+my_file.writelines([
+      'cyclops\n',
+      'Bishop\n',
+      'Night Crawler\n'
+])
+my_file.seek(0)
+print(my_file.read())
+my_file.close()
